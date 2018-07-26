417e4e24264fe23b6881526e4dc58483154ad8e3
withouttalent <33684530+withouttalent@users.noreply.github.com>
2018-07-26 19:04:14 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 var path = require("path")
+var proxy = require('http-proxy-middleware');
+
+
 
 app.use(["/assets", "/user/assets", "/user/message/assets"], express.static(path.join(__dirname, "/assets")))
+app.use(["/api/v0", "/media"], proxy({target:"http://206.189.62.203:8000", changeOrigin: true}))
 app.set('trust proxy', 'loopback')
 
 
